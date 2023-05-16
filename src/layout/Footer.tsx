6ff13ed98c6133a1366e46dae3c51e63ed8eda12
최대열 <dychoi@brainednet.com>
2023-05-16 13:27:44 +0900

    layout설정

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer: React.FC = () => {
     return (
-        <footer>
-            <p>© 2023 My App</p>
+        <footer className="footer">
+            <div className="footer-info">
+                <p>연락처: 123-456-7890</p>
+                <p>이메일: example@example.com</p>
+            </div>
+            <div className="footer-menu">
+                <p>농수산물 가격 검색</p>
+                <p>쇼핑</p>
+                <p>자유게시판</p>
+            </div>
         </footer>
     );
 };
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'; // CSS 파일을 import 합니다.
 
 const Header: React.FC = () => {
     return (
-        <header>
-
-            <div>
-                <Link to="/login">로그인</Link>
-                <Link to="/signup">회원가입</Link>
-                <Link to="/find">ID/PW 찾기</Link>
-            </div>
-            <h1>My App</h1>
-        </header>
+        <div>
+            <header className="header">
+                <div className="header-links">
+                    <Link to="/login">로그인</Link>
+                    <Link to="/signup">회원가입</Link>
+                    <Link to="/find">ID/PW 찾기</Link>
+                </div>
+            </header>
+            <nav className="navbar">
+                <Link to="/">로고</Link>
+                <Link to="/prices">농수산물 가격 검색</Link>
+                <Link to="/shopping">쇼핑</Link>
+                <Link to="/forum">자유게시판</Link>
+            </nav>
+        </div>
     );
 };
 
